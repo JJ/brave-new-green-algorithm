410f04f047114aac45eebaba7edb98e92e4154af
JJ Merelo <jjmerelo@gmail.com>
2026-05-16 20:02:57 +0200

    #70 interpreting and finding clusters

--- script/europar-2026-poster-create-file.R
+++ script/europar-2026-poster-create-file.R
@@ -7,12 +7,11 @@ europar_poster_dataset <- rbind(europar_test,europar_taskset,europar_taskset_die
 europar_poster_dataset$run <- ifelse( startsWith(europar_poster_dataset$work, "base"), "B", "W")
 
 europar_poster_dataset$alpha <- NULL
-europar_poster_dataset$work <- NULL
 
 europar_poster_dataset$power <- europar_poster_dataset$PKG/europar_poster_dataset$seconds
 summary(europar_poster_dataset$power)
 
-save(file="data/europar_poster_datset.rds",europar_poster_dataset)
+write.csv(europar_poster_dataset, file="data/europar_poster_dataset.csv")
 
 library(ggplot2)
 library(ggExtra)
