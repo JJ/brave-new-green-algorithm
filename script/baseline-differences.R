2dec8e23159424ad44177d9f31bcdbfdf77e8492
JJ Merelo <jjmerelo@gmail.com>
2026-07-05 10:16:53 +0200

    #97 there are a lot of differences
    Over all temperature

--- script/baseline-differences.R
+++ script/baseline-differences.R
@@ -11,3 +11,29 @@ load("data/icsm_hot_first.rds")
 hot_first_base <- icsm_hot_first[ startsWith(icsm_hot_first$work,"base"), ]
 hot_first_base$work <- "hot-first"
 
+regular_vs_hot_first <- rbind(regular_base,hot_first_base)
+
+library(ggplot2)
+
+ggplot(regular_vs_hot_first, aes(x=seconds, y=PKG, group=work, color=work))+
+  geom_point()+theme_minimal()
+
+regular_vs_hot_first$dimension <- as.factor(regular_vs_hot_first$dimension)
+regular_vs_hot_first$population_size <- as.factor(regular_vs_hot_first$population_size)
+regular_vs_hot_first_time_model <- glm( seconds ~ work*dimension*population_size, data=regular_vs_hot_first)
+
+regular_vs_hot_first_temp1_model <- glm( initial_temp_1 ~ work*dimension*population_size, data=regular_vs_hot_first)
+regular_vs_hot_first_temp2_model <- glm( initial_temp_2 ~ work*dimension*population_size, data=regular_vs_hot_first)
+
+regular_vs_hot_first$residual_seconds <- residuals(regular_vs_hot_first_time_model)
+regular_vs_hot_first$residual_i_temp_1 <- residuals(regular_vs_hot_first_temp1_model)
+regular_vs_hot_first$residual_i_temp_2 <- residuals(regular_vs_hot_first_temp2_model)
+
+regular_vs_hot_first_PKG_model <- glm( PKG ~ work*dimension*population_size +
+                                        residual_i_temp_1*residual_i_temp_2+
+                                        I(residual_i_temp_1^2)*I(residual_i_temp_2^2)+
+                                        residual_seconds + I(residual_seconds^2),
+                                       data=regular_vs_hot_first)
+
+ggplot(regular_vs_hot_first, aes(x=initial_temp_1, y=initial_temp_2, group=work, color=work))+
+  geom_point()+theme_minimal()
